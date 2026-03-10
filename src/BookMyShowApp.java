8adb2d19ad9296c96435ec39c3bba3f9edf78191
Piyasha-chakraborty <piyashachakraborty17102003@gmail.com>
2026-03-10 09:11:13 +0530

    UC3 Centralized inventory using HashMap

--- src/BookMyShowApp.java
+++ src/BookMyShowApp.java
@@ -1,13 +1,85 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.HashMap;
+import java.util.Map;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class BookMyShowApp {
+
+    static abstract class Room {
+
+        protected int beds;
+        protected int size;
+        protected double price;
+
+        public Room(int beds, int size, double price) {
+            this.beds = beds;
+            this.size = size;
+            this.price = price;
+        }
+
+        public void displayDetails() {
+            System.out.println("Beds: " + beds);
+            System.out.println("Size: " + size + " sqft");
+            System.out.println("Price per night: " + price);
+        }
+    }
+
+    static class SingleRoom extends Room {
+        public SingleRoom() {
+            super(1, 250, 1500.0);
+        }
+    }
+
+    static class DoubleRoom extends Room {
+        public DoubleRoom() {
+            super(2, 400, 2500.0);
+        }
+    }
+
+    static class SuiteRoom extends Room {
+        public SuiteRoom() {
+            super(3, 750, 5000.0);
+        }
+    }
+
+    static class RoomInventory {
+
+        private Map<String, Integer> roomAvailability;
+
+        public RoomInventory() {
+            roomAvailability = new HashMap<>();
+            roomAvailability.put("Single", 5);
+            roomAvailability.put("Double", 3);
+            roomAvailability.put("Suite", 2);
+        }
+
+        public Map<String, Integer> getAvailability() {
+            return roomAvailability;
+        }
+
+        public void updateAvailability(String type, int count) {
+            roomAvailability.put(type, count);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        RoomInventory inventory = new RoomInventory();
+
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        System.out.println("Hotel Room Inventory Status\n");
+
+        System.out.println("Single Room:");
+        single.displayDetails();
+        System.out.println("Available Rooms: " + inventory.getAvailability().get("Single"));
+
+        System.out.println("\nDouble Room:");
+        doubleRoom.displayDetails();
+        System.out.println("Available Rooms: " + inventory.getAvailability().get("Double"));
+
+        System.out.println("\nSuite Room:");
+        suite.displayDetails();
+        System.out.println("Available Rooms: " + inventory.getAvailability().get("Suite"));
     }
 }
