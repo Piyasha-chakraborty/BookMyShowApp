565df5fa09b1fb35e69aec5e88c86d4423978186
Piyasha Chakraborty <piyashachakraborty17102003@gmail.com>
2026-03-17 09:37:32 +0530

    UC8 implemented

---
+++ src/BookMyShowApp.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+class Reservation {
+
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+class BookingHistory {
+
+    private List<Reservation> confirmedReservations;
+
+    public BookingHistory() {
+        confirmedReservations = new ArrayList<>();
+    }
+
+    public void addReservation(Reservation reservation) {
+        confirmedReservations.add(reservation);
+    }
+
+    public List<Reservation> getConfirmedReservations() {
+        return confirmedReservations;
+    }
+}
+
+class BookingReportService {
+
+    public void generateReport(BookingHistory history) {
+
+        System.out.println("Booking History and Reporting\n");
+
+        for (Reservation r : history.getConfirmedReservations()) {
+            System.out.println("Guest: " + r.getGuestName() +
+                    ", Room Type: " + r.getRoomType());
+        }
+    }
+}
+
+public class BookMyShowApp {
+
+    public static void main(String[] args) {
+
+        BookingHistory history = new BookingHistory();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vannathi", "Suite");
+
+        history.addReservation(r1);
+        history.addReservation(r2);
+        history.addReservation(r3);
+
+        BookingReportService reportService = new BookingReportService();
+        reportService.generateReport(history);
+    }
+}
